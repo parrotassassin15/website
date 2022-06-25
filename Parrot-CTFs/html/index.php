82ba58d34f166e94a1cb1915e79759bea407b3e5
parrot <54908247+parrotassassin15@users.noreply.github.com>
2022-06-25 16:33:11 -0400

    upload index.php

---
+++ Parrot-CTFs/html/index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="keywords" content="parrot ctfs, hacking, live hacker, live hacking, hack, news hacker, newsroom vips, hacking the games, hackthisgame, hackthissite, hack this site, tryhack, gaming hacks, hacker hosting, hosting, parrot pentest, parrot labs, parrot, oscp, oscp training lab, mitre, owasp, tryhackme, hackthebox, picoctf, capture the flag, ctf, hacking, coding, hacker, ethical hacking, sysadmin, soc analyst, developer, red team, blue team, purple team, infosec, new ctf platform, ctf writeups, crtp, ad, active directory, com, hack, pentest+, crtp, lab env, login,parrot ctf, hack the box, hack"/>
+      <meta name="description" content="Parrot CTFs - A CTF platform for all fields of IT - OWASP, CRTP & OSCP Based Training Labs">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+      <link rel="shortcut icon" href="https://parrot-ctfs.com/favicon.ico" />
+      <title>Parrot CTFs | Home</title>
+      <!-- added in CSS -->
+      <link rel="stylesheet" href="css/style2-index.css">
+      <meta name="robots" content="all,follow">
+      <!-- Choices.js-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
+      <link rel="stylesheet" href="vendor/choices.js/public/assets/styles/choices.min.css">
+      <!-- Google fonts - Muli-->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+      <!-- theme stylesheet-->
+      <link rel="stylesheet" href="/css/style.default.css" id="theme-stylesheet">
+      <!-- Custom stylesheet - for your changes-->
+      <link rel="stylesheet" href="/css/custom.css">
+      <!-- Favicon-->
+      <link rel="shortcut icon" href="/img/favicon.ico">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+      <script src="https://kit.fontawesome.com/0583ca94dd.js" crossorigin="anonymous" type="1d671b5efc0baefaf4f46f02-text/javascript"></script>
+      <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <body>
+      <header class="header">
+         <nav class="navbar navbar-expand-lg py-3 bg-dash-dark-2 border-bottom border-dash-dark-1 z-index-10">
+            <div class="search-panel">
+               <div class="search-inner d-flex align-items-center justify-content-center">
+                  <div class="close-btn d-flex align-items-center position-absolute top-0 end-0 me-4 mt-2 cursor-pointer">
+                     <span>Close </span>
+                     <svg class="svg-icon svg-icon-md svg-icon-heavy text-gray-700 mt-1">
+                        <use xlink:href="#close-1"> </use>
+                     </svg>
+                  </div>
+                  <div class="row w-100">
+                     <div class="col-lg-8 mx-auto">
+                        <form class="px-4" id="searchForm" action="#">
+                           <div class="input-group position-relative flex-column flex-lg-row flex-nowrap">
+                              <input class="form-control shadow-0 bg-none px-0 w-100" type="search" name="search" placeholder="What are you searching for...">
+                              <button class="btn btn-link text-gray-600 px-0 text-decoration-none fw-bold cursor-pointer text-center" type="submit">Search</button>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="container-fluid d-flex align-items-center justify-content-between py-1">
+               <div class="navbar-header d-flex align-items-center">
+                  <a class="navbar-brand text-uppercase text-reset" href="index.php">
+                     <div class="brand-text brand-big"><img src="img/parrot-ctf-removebg-preview.png" height="62.5"></div>
+                  </a>
+               </div>
+               <ul class="list-inline mb-0">
+                  </li>
+                  </li>
+                  <li class="list-inline-item merch px-lg-2">
+                     <a class="nav-link text-sm text-reset px-1 px-lg-0" href="https://shop.parrot-ctfs.com">
+                        <span class="d-none d-sm-inline-block">Merchandise</span>
+                  <li class="list-inline-item merch px-lg-2">                 <a class="nav-link text-sm text-reset px-1 px-lg-0" href="https://discord.parrot-ctfs.com"> <span class="d-none d-sm-inline-block">Join our Discord</span>
+                  <li class="list-inline-item merch px-lg-2">                 <a class="nav-link text-sm text-reset px-1 px-lg-0" href="login.php"> <span class="d-none d-sm-inline-block">Login</span>
+                  <use xlink:href="#disable-1"> </use>
+                  </svg></a></li>
+                  <li class="list-inline-item logout px-lg-2">
+                     <a class="nav-link text-sm text-reset px-1 px-lg-0" id="logout" href="signup.php">
+                        <span class="d-none d-sm-inline-block">Signup</span>
+                        <use xlink:href="#disable-1"> </use>
+                        </svg>
+                     </a>
+                  </li>
+                  <li class="list-inline-item logout px-lg-2">
+                     <a class="nav-link text-sm text-reset px-1 px-lg-0" id="logout" href="tos-rules.php">
+                        <span class="d-none d-sm-inline-block">ToS/Rules</span>
+                        <use xlink:href="#disable-1"> </use>
+                        </svg>
+                     </a>
+                  </li>
+                  <li class="list-inline-item logout px-lg-2">
+                     <a class="nav-link text-sm text-reset px-1 px-lg-0" id="logout" href="https://forum.parrot-ctfs.com">
+                        <span class="d-none d-sm-inline-block">User Forum</span>
+                        <use xlink:href="#disable-1"> </use>
+                        </svg>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+      <center>
+      <body>
+         <br>
+         <div class="logo_img"> <img class="logo_img" src="/img/parrotpentest-logo.png" alt="logo" /> </div>
+         <hr color="white">
+         <h1>
+            <br>
+            OWASP, CRTP & OSCP Based Training Labs 
+            <h2 class="header2_users">
+            <br>
+            <a href="signup.php"> Join our 100+ users and advance your cyber security skills today!</a>
+            <h2>
+         </h1>
+         <br>
+         <br>
+         <br>
+         <center>
+            <hr color="white" width="75%">
+         </center>
+         <br>
+         <h2> Hands On Labs For All Fields Of IT </h2>
+         <br>
+         <br>
+         <br>
+         <h2> Lab Machines: </h2>
+         <br>
+         <div> <img class="first_image" src="/img/lab_machine.png"> </div>
+         <br>
+         <br>
+         <h2> Active Directory Labs: (coming soon!) </h2>
+         <br>
+         <br>
+         <div> <img class="first_image" src="/img/coming-soon.png"> </div>
+         <br>
+         <br> 
+         <h2> Third Party CTF Hosting: (coming soon!) </h2>
+         <br>
+         <div> <img class="first_image" src="/img/coming-soon.png"> </div>
+         <br>
+         <br>
+         <h2> Friendly User Forums: </h2>
+         <br>
+         <div> <img class="first_image" src="/img/user-forums.png"> </div>
+         <br>
+         <br>
+         <section class="pt-0">
+            </div>
+            </div>
+         </section>
+         <!-- Page Footer-->
+         <!-- BACKGROUND MUSIC -->
+         <audio src="/music/audio-main-page.mp3" autoplay loop></audio>
+         </div>
+         </div>
+         <!-- JavaScript files-->
+         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+         <script src="vendor/just-validate/js/just-validate.min.js"></script>
+         <script src="vendor/chart.js/Chart.min.js"></script>
+         <script src="vendor/choices.js/public/assets/scripts/choices.min.js"></script>
+         <script src="js/charts-home.js"></script>
+         <script src="js/charts-custom.js"></script>
+         <!-- Main File-->
+         <script src="js/front.js"></script>
+         <script>
+            // ------------------------------------------------------- //
+            //   Inject SVG Sprite - 
+            //   see more here 
+            //   https://css-tricks.com/ajaxing-svg-sprite/
+            // ------------------------------------------------------ //
+            function injectSvgSprite(path) {
+            
+                var ajax = new XMLHttpRequest();
+                ajax.open("GET", path, true);
+                ajax.send();
+                ajax.onload = function(e) {
+                var div = document.createElement("div");
+                div.className = 'd-none';
+                div.innerHTML = ajax.responseText;
+                document.body.insertBefore(div, document.body.childNodes[0]);
+                }
+            }
+            
+            injectSvgSprite('https://bootstraptemple.com/files/icons/orion-svg-sprite.svg'); 
+            
+            
+            
+         </script>
+         <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+   </body>
+</html>
